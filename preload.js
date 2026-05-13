b4deecf1e4bc2864661dce3d3ad8c53332d5dbeb
Peter Hagman <peter.hagman@sedesign.se>
2026-05-13 21:24:54 +0200

    feat: preload context bridge

---
+++ preload.js
@@ -0,0 +1,31 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('scythe', {
+  // Scanning
+  startScan: (targetIds) => ipcRenderer.invoke('scan:start', targetIds),
+  abortScan: () => ipcRenderer.send('scan:abort'),
+
+  // Estimates
+  estimateAll: () => ipcRenderer.invoke('estimate:all'),
+
+  // Deletion
+  deleteItems: (items) => ipcRenderer.invoke('delete:items', items),
+  emptyTrash: () => ipcRenderer.invoke('trash:empty'),
+
+  // Finder
+  revealInFinder: (targetPath) => ipcRenderer.invoke('finder:reveal', targetPath),
+
+  // Event listeners
+  onScanProgress: (cb) => ipcRenderer.on('scan:progress', (_, data) => cb(data)),
+  onScanComplete: (cb) => ipcRenderer.on('scan:complete', (_, data) => cb(data)),
+  onEstimateResult: (cb) => ipcRenderer.on('estimate:result', (_, data) => cb(data)),
+  onDeleteProgress: (cb) => ipcRenderer.on('delete:progress', (_, data) => cb(data)),
+  onDeleteComplete: (cb) => ipcRenderer.on('delete:complete', (_, data) => cb(data)),
+
+  // Listener cleanup
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+
+  // Persistent store
+  storeGet: (key) => ipcRenderer.invoke('store:get', key),
+  storeSet: (key, value) => ipcRenderer.invoke('store:set', key, value),
+})
